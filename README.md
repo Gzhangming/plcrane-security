92254896cb7c19357b77802645b50768ec3d0f8a
Gzhangming <1960881551@qq.com>
2019-11-15 13:44:58 +0800

    项目生成

---
+++ README.md
@@ -0,0 +1,25 @@
+##  **技术栈**
+    1、spring io   Brussels-SR4
+    1、SpringCloud Dalston.SR2
+    2、SpringBoot 
+    3、Oauth2   权限验证
+    4、Redis    缓存
+    5、Mysql    数据库
+
+
+## **代码模块划分**
+    ├── plcrane-security   主
+    ├── plcrane-security-core   核心
+    ├── plcrane-security-app     
+    ├── plcrane-security-browser
+    ├── plcrane-security-demo   展示模块
+    ├── prcrane-security-admin  总后台管理
+    ├── PRCrane-security-pc     官网首页
+    ├── 
+    ├── 
+    ├── 
+    └── 
+    
+    ## **开发环境**
+        JDK 1.8
+        Redis 3.2及以上
---
+++ plcrane-security-demo/src/main/java/com/plcrane/DemoApplication.java
@@ -0,0 +1,45 @@
+package com.plcrane;//
+//                            _ooOoo_
+//                           o8888888o
+//                           88" . "88
+//                           (| -_- |)
+//                           O\  =  /O
+//                        ____/`---'\____
+//                      .'  \\|     |//  `.
+//                     /  \\|||  :  |||//  \
+//                    /  _||||| -:- |||||-  \
+//                    |   | \\\  -  /// |   |
+//                    | \_|  ''\---/''  |   |
+//                    \  .-\__  `-`  ___/-. /
+//                  ___`. .'  /--.--\  `. . __
+//               ."" '<  `.___\_<|>_/___.'  >'"".
+//              | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//              \  \ `-.   \_ __\ /__ _/   .-` /  /
+//         ======`-.____`-.___\_____/___.-`____.-'======
+//                            `=---='
+//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//                      佛祖保佑       永无BUG
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Creator: zhang.jiaming
+ * @CreationTime: 2019-11-15 10:59
+ * @Description: 启动类
+ **/
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("hello")
+    public String hello(){
+        return "你好 spring security_____";
+    }
+
+}
